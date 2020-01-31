ecb39b42c6842ddad05b0035b9bb374af724270a
sekoudosso82 <sekou.dosso82@gmail.com>
2020-01-30 22:49:12 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,42 @@
 require 'pry'
 
 class String
+  def initialize ()
+  end 
 
   def sentence?
+    # teststr.split('').last
+
+    self.split('').last== "." ? true : false 
     
   end
 
   def question?
+    self.split('').last== "?" ? true : false
 
   end
 
   def exclamation?
+    self.split('').last== "!" ? true : false
 
   end
 
   def count_sentences
-
+    count_sentence  = self.split
+    sum = 0 
+    counter = 0 
+    while counter < count_sentence.length
+      if count_sentence[counter][-1]== "." 
+        sum += 1
+      end
+      if count_sentence[counter][-1]== "?"
+        sum += 1
+      end
+      if count_sentence[counter][-1]== "!"
+        sum += 1
+      end
+      counter += 1
+    end   
+    sum
   end
 end
